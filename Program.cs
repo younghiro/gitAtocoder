6fa5aa0199da10fd417cef9d85e13c969a9e53e2
younghiro <younghiromu112010@icloud.com>
2022-06-19 17:57:31 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitAtcoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //atcoderで解いた問題をgitやgithubにアップロードしていきます。
+        }
+    }
+}
